2393f9b579fa5c862aca22dfd00e11700048fc8c
InnaTarasyan <innatarasyanmail@gmail.com>
2016-03-10 18:05:20 +0400

    First Commit

--- home.php
+++ home.php
@@ -1,3 +1,78 @@
-<?php
-echo "Hello";
-?>
+<html>
+<head>
+    <link>
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+    <link href="css/styles.css" rel="stylesheet" type="text/css" >
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="screen"></div>
+<div class="screen1"></div>
+<div class="screen3"></div>
+<div class="screen4"></div>
+
+
+<div class="Layer_16"></div>
+
+<div class="bg4"></div>
+<div class="_customer">@customer</div>
+<div class="det_er_dejligt_med_en_app_der_hjælper_med_reseg">Det er dejligt med en app der hjælper med resegaranti så simpel</div>
+<div class="Layer_16"></div>
+
+
+
+<div class="map">
+</div>
+<div class="text_1">
+
+<div class="sidste_nyt">Sidste nyt</div>
+<div class="Her_kan_du_løbende_følge_med_i_relevante_nyhede">Her kan du løbende følge med i relevante nyheder angående DSB R</div>
+<div class="Omkring_os">Omkring os</div>
+<div class="Vores_målsætning_er_at_gøre_DSB_Rejsegaranti_ti">Vores målsætning er at gøre DSB Rejsegaranti tilgængelig for al</div>
+</div>
+
+<div class="text_2">
+<div class="Blog">Blog</div>
+<div class="iconMap1"></div>
+<div class="DSB_forsinkelser_–_DSB_rejsegaranti_kan_komme_i">DSB forsinkelser – DSB rejsegaranti kan komme i spil September </div>
+<div class="iconMap2"></div>
+<div class="DSB1_forsinkelser_–_DSB_rejsegaranti_kan_komme_i">DSB forsinkelser – DSB rejsegaranti kan komme i spil September </div>
+<div class="_Sikre_kompensation_Simpel_ansøgning_Flot_">• Sikre kompensation <br/>• Simpel ansøgning<br/> • Flot design<br/> • Gratis </div>
+<div class="Vores_målsætning">Vores målsætning:</div>
+</div>
+
+<div class="_1">
+<div class="Omkring_os__E_Opinion_er_specialiserede_indenfo">Omkring os<br/>
+    E-Opinion er specialiserede indenfor<br/>
+    it-outsourcin</div>
+</div>
+<div class="_2">
+<div class="Kategori________DSB_Rejsegaranti__Archives_____">
+    Kategori<br/>
+    DSB Rejsegaranti<br/>
+    Archives<br/>
+    Septembe<br/>
+</div>
+</div>
+<div class="_3">
+    <div class="bg1"></div>
+    <div class="DSB_Rejsegaranti">DSB Rejsegaranti</div>
+    <div class="Download_Now">Download Now</div>
+</div>
+<div class="_4">
+<div class="search">
+<div class="bg3"></div>
+</div>
+<div class="søg">Søg</div>
+</div>
+<div class="bg">
+</div>
+<div class="footer_2">
+<div class="Copyright_DSB_Rejsegaranti_2015__All_rights_res">Copyright DSB Rejsegaranti 2015. All rights reserved.</div>
+ <div class="Rectangle_5"></div>
+</div>
+
+</body>
+</html>
