23af95bfd121e8a9fdb37dbc7bb5c9236d7ea875
InnaTarasyan <innatarasyanmail@gmail.com>
2016-03-11 18:01:00 +0400

    Main source code is inside index.php

--- home.php
+++ home.php
@@ -2,24 +2,68 @@
 <head>
     <link>
     <script src="js/jquery-1.12.1.min.js"></script>
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+   <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+    <link href="css/styles2.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css" >
-    <script src="js/bootstrap.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+
 </head>
 <body>
+<div class="social_icons">
+    <div class="layer_44"></div>
+    <div class="layer_43"></div>
+    <div class="layer_42"></div>
+    <div class="layer_41"></div>
+    <div class="layer_40"></div>
+</div>
 
-<div class="screen"></div>
+<div class="iPhone_6_White"></div>
+<div class="black_screen_copy"></div>
+<div class="Left_Hand___iPhone"></div>
+
+
+<div class="download_now1">Download Now</div>
+<div class="bg7"></div>
+<div class="top_logo"></div>
+<div class="logo"></div>
+<div class="DSB4_Basis_Rejsetidsgaranti">DSB Basis Rejsetidsgaranti</div>
+<div class="Kompensation_ved_forsinkelse">Kompensation ved forsinkelse</div>
+<div class="bg6"></div>
+
+<div class="new"></div>
+
+<div class="text_5">
+<div class="DSB_Rejsetidsgaranti__Få_kompensation">DSB Rejsetidsgaranti. Få kompensation.</div>
+<div class="Hvis_du_på_en_rejse_er_blevet_forsinket_mere_en">Hvis du på en rejse er blevet forsinket mere end 30 minutter, kan du få kompensation via DSB Basis Rejsetidsgaranti, når du har rejst med DSB fjern- eller regional tog i Danmark med billet, klippekort, rejsekort eller Bus & Tog Årskort.</div>
+<div class="bg5"></div>
+</div>
+
+<div class="screens">
+    <div class="Vi_har_gjort_det_simplet">Vi har gjort det simplet</div>
+    <div class="Kompensation">Kompensation</div>
+    <div class="Rejseoplysninger">Rejseoplysninger</div>
+    <div class="Personoplysninger">Personoplysninger</div>
+    <div class="icon10"></div><div class="money"></div><div class="Ellipse_1"></div>
+    <div class="icon11"></div><div class="person"></div><div class="Ellipse_2"></div>
+    <div class="icon12"></div><div class="train"></div><div class="Ellipse_3"></div>
+    <div class="icon13"></div><div class="ticket"></div><div class="Ellipse_4"></div>
+
+    <div class="Type">Type</div>
+
 <div class="screen1"></div>
-<div class="screen3"></div>
 <div class="screen4"></div>
+<div class="screen"></div>
+<div class="screen3"></div>
+    <div class="bg8"></div>
+    <div class="icon4"></div>
+    <div class="Download_NowButton">Download Now</div>
+</div>
 
 
 <div class="Layer_16"></div>
-
 <div class="bg4"></div>
 <div class="_customer">@customer</div>
 <div class="det_er_dejligt_med_en_app_der_hjælper_med_reseg">Det er dejligt med en app der hjælper med resegaranti så simpel</div>
-<div class="Layer_16"></div>
 
 
 
@@ -46,27 +90,48 @@
 <div class="_1">
 <div class="Omkring_os__E_Opinion_er_specialiserede_indenfo">Omkring os<br/>
     E-Opinion er specialiserede indenfor<br/>
-    it-outsourcin</div>
+    it-outsourcing<br/>
+    <br/>
+    E-Opinion ApS<br/>
+    Søren Frichs Vej 40A<br/>
+    8230 Åbyhøj<br/>
+    Mobil: 22 14 54 56<br/>
+    Internet: E-Opinion<br/>
+    E-mail: kontakt@e-opinion.dk
+</div>
 </div>
 <div class="_2">
 <div class="Kategori________DSB_Rejsegaranti__Archives_____">
+        <div class="Layer_19_copy"></div>
+        <div class="Layer_18_copy_2"></div>
+
     Kategori<br/>
     DSB Rejsegaranti<br/>
+
     Archives<br/>
+
     Septembe<br/>
+
+
 </div>
 </div>
 <div class="_3">
+    <div class="lines">
     <div class="bg1"></div>
     <div class="DSB_Rejsegaranti">DSB Rejsegaranti</div>
     <div class="Download_Now">Download Now</div>
+    <div class="DBS_Rejsegaranti7">DBS Rejsegaranti</div>
+     </div>
 </div>
 <div class="_4">
 <div class="search">
 <div class="bg3"></div>
+<div class="iconSearch"></div>
 </div>
 <div class="søg">Søg</div>
 </div>
+
+
 <div class="bg">
 </div>
 <div class="footer_2">
---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <link>
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+    <link rel="stylesheet" href="css/MyStyles.css" type="text/css">
+    <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="navbar navbar-inverse navbar-fixed-top bg6" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!--<a class="navbar-brand" href="#">Project name</a>-->
+            <div class="navbar-brand logo"></div>
+        </div>
+        <div class="navbar-collapse collapse">
+            <div class="navbar-right">
+                <ul class="nav navbar-nav">
+                <li><div class="Layer_44"></div></li>
+                <li><div class="Layer_43"></div></li>
+                    <li><div class="Layer_42"></div></li>
+                    <li><div class="Layer_41"></div></li>
+                    <li><div class="Layer_40"></div></li>
+                </ul>
+            </div>
+
+        </div><!--/.navbar-collapse -->
+    </div>
+</div>
+</body>
+</html>
+
+
