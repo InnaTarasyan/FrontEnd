6d590b93e196279c127326fc1fd572900e308460
InnaTarasyan <innatarasyanmail@gmail.com>
2016-03-16 17:38:33 +0400

    Placed image slider instead of image in the main menu

---
+++ top_content.php
@@ -0,0 +1,76 @@
+<div class="carousel-content" >
+
+    <div class="row">
+        <div class="col-md-8 center-block visible-lg visible-md">
+
+
+            <div  style="font-size: 20px;font-family: 'OpenSans';
+           position: relative;padding-top: 250.718px;
+           padding-left: 312.938px;
+           color: rgb(255, 255, 255);
+           line-height: 1.2;
+           float: none;
+           margin: 0 auto;">
+                <div style=" font-size: 40px;font-weight: bold;">
+                    DSB Basis Rejsetidsgaranti</div>
+                <div style="text-transform: uppercase">
+                    Kompensation ved forsinkelse<br/><br/>
+
+                    <div style="border-style: solid;border-width: 2px;
+                           border-color: rgb(255, 255, 255);
+                           background-color: rgba(1, 0, 0, 0.161);
+                           width: 229px;
+                           height: 53px;
+                           text-align: center;
+                           ">
+                        Download Now
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-md-4" >
+
+            <img src="images/top/iPhone%206%20White.png" class="imgimg"
+            />
+
+        </div>
+    </div>
+
+
+    <div class="row visible-xs visible-sm" style="background-color: rgb(219, 12, 68);text-align: center;padding-bottom: 20px;
+               ">
+
+        <div style="display: table;
+                margin: 0 auto;">
+
+            <div style="font-size: 14px;
+                    font-family: 'OpenSans';
+                    color: rgb(255, 255, 255);
+                    line-height: 1.2;">
+                <div style=" font-size: 24px;">
+                    DSB Basis Rejsetidsgaranti</div><br/>
+
+                <div style="text-transform: uppercase"> Kompensation ved forsinkelse</div>
+                <br/>
+                <div style="border-style: solid;
+                  border-width: 2px;
+                  border-color: rgb(255, 255, 255);
+                  background-color: rgba(1, 0, 0, 0.161);
+                  width: 225px;
+                  height: 52px;
+                  margin: 0 auto;
+                  padding: 10px;
+                  position: relative;
+                  text-align: center;
+                  text-transform: uppercase">
+                    Download Now
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</div>
